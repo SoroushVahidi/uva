2c78150cd97ebf7f0b9dde5b81ec3f0f2a3bd0d0
Soroush Vahidi <54101845+SoroushVahidi@users.noreply.github.com>
2020-08-13 14:42:48 +0430

    Add files via upload
    10523 uva solution

--- Main.java
+++ Main.java
@@ -1,74 +1,58 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-package pkg10213;
+package pkg10523;
 import java.math.BigInteger; 
 import java.util.Scanner; 
-
 
 /**
  *
  * @author sorush
  */
-class Main{
-  
-    static BigInteger tarkib4(long N) 
+public class Main {
+     static BigInteger tavan(long n,long m) 
     { 
-        // Initialize result 
-        if(N<4)
-             return (new BigInteger("0"));
         
-        BigInteger f = new BigInteger("1"); // Or BigInteger.ONE 
-  
-        // Multiply f with 2, 3, ...N 
-        for (long i=N; i >N-4; i--) 
-            f = f.multiply(BigInteger.valueOf(i)); 
-        f=f.divide(new BigInteger("24"));
-  
-        return f; 
-    } 
-    static BigInteger tarkib2(long N) 
-    { 
         // Initialize result 
-        if(N<2)
-             return (new BigInteger("0"));
-        
         BigInteger f = new BigInteger("1"); // Or BigInteger.ONE 
-  
+        if(m==0)
+              return f;
+        if(m==1){
+            BigInteger u=BigInteger.valueOf(n);
+            return u;
+            
+        }
+        BigInteger ans=tavan(n,m/2);
+        ans=ans.multiply(ans);
+        if(m%2==1)
+            ans=ans.multiply(BigInteger.valueOf(n));
+        return ans;
         // Multiply f with 2, 3, ...N 
-        for (long i=N; i >N-2; i--) 
-            f = f.multiply(BigInteger.valueOf(i)); 
-        f=f.divide(BigInteger.valueOf(2));
-        return f; 
     } 
-  
 
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         // TODO code application logic here
         Scanner myInput = new Scanner( System.in );  
-        int N  ;
-        N=myInput.nextInt();
+        long N,A  ;
+        
         int t=0;
-      //  BigInteger mod = new BigInteger("10000000000000007");
-        while(N>0){
-            t++;
-            N--;  
-            long r;
-            r=myInput.nextInt();
+      
+        while(myInput.hasNext()){
             
-            
-            BigInteger str = ((tarkib4(r).add(tarkib2(r))).add(BigInteger.valueOf(1)));
-         
+            N=myInput.nextInt();  
+            A=myInput.nextInt();
+            BigInteger str = new BigInteger("0");
+            for(long i=1;i<=N;i++)
+                   str=str.add(tavan(A,i).multiply(BigInteger.valueOf(i)));
             System.out.println(str); 
         }
     }    
+        // TODO code application logic her
+    
 }
-
